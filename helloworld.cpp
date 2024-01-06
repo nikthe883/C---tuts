f9d069fb3fb8620eb4b4557d3c9bc36481a4b1b0
nikthe883 <popmihaylov@gmail.com>
2024-01-06 17:38:08 +0200

    vars

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+
+int main(){
+
+    std::cout << "Supp, bro" << "\n";
+    std::cout << "Some writing" << "\n";
+
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main(){
+
+    int x; // declaration
+    x = 10; // assignment
+    int y = 6;
+
+    std:: cout << x << "\n";
+    std:: cout << y << "\n";
+    std:: cout << x + y << "\n";
+
+    // integers
+    int age = 29;
+
+    // doubles with decimap point up to 15 digits
+    double price = 5.60666666666666;
+
+    // float up to 7 digits
+    float test = 1.12345678;
+    std:: cout << test << "\n";
+
+
+    // single character
+    char grade = 'A';
+    std:: cout << grade << "\n";
+
+    //boolean (true, false)
+    bool student = true;
+    bool forSale = false;
+    std:: cout << student << "\n";
+
+    // string 
+    std::string name = "Peter";
+    std:: cout << name << "\n";
+
+
+    return 0;
+}
